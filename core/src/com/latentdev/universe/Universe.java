298177ad11b879f5b71346ba17b1195661fd081a
Nick <latentdev@gmail.com>
2016-01-09 05:15:12 -0800

    art commit

--- core/src/com/latentdev/universe/Universe.java
+++ core/src/com/latentdev/universe/Universe.java
@@ -2,6 +2,7 @@ package com.latentdev.universe;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,21 +11,25 @@ public class Universe extends ApplicationAdapter {
 	SpriteBatch batch;
 	Entity Main;
 	Entity [] level;
+	Music Theme;
 
-	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		Main= new Entity(0,128,0,0,"steven-256.png");
+		Theme = Gdx.audio.newMusic(Gdx.files.internal("gems.ogg"));
 		level= new Entity[7];
 
-		level[0]= new Entity(0,0,0,5,"Battleground.png");
+		level[0]= new Entity(0,0,0,10,"Battleground.png");
 		level[1]= new Entity(0,0,0,level[0].speed*.5,"clouds.png");
 		level[2]= new Entity(0,0,0,level[0].speed*.25,"sky.png");
 		level[3]= new Entity(0,0,0,level[0].speed*1.5,"Hammer.png");
 		level[4]= new Entity(1024,0,0,level[0].speed*1.5,"Sword.png");
 		level[5]= new Entity(2048,0,0,level[0].speed*1.5,"hatchet.png");
 		level[6]= new Entity(0,0,0,level[0].speed*2,"Foreground.png");
+		Theme.setLooping(true);
+		Theme.play();
+
 	}
 
 	@Override
